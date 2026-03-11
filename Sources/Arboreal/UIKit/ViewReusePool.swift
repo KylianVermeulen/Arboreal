3cbc38c5d32169fb4b600e21bbd84fb1a3e2f10b
Kylian Vermeulen <email@kylianvermeulen.com>
2026-03-11 07:30:10 +0100

    Add UIKit view infrastructure with scroll view and cell reuse

---
+++ Sources/Arboreal/UIKit/ViewReusePool.swift
@@ -0,0 +1,48 @@
+import UIKit
+
+@MainActor
+final class ViewReusePool<Cell: UIView> {
+    private var available: [Cell] = []
+    private var inUse: [AnyHashable: Cell] = [:]
+    private let factory: () -> Cell
+
+    init(factory: @escaping () -> Cell) {
+        self.factory = factory
+    }
+
+    func dequeue(for key: AnyHashable) -> Cell {
+        if let existing = inUse[key] {
+            return existing
+        }
+        let cell: Cell
+        if let recycled = available.popLast() {
+            cell = recycled
+        } else {
+            cell = factory()
+        }
+        inUse[key] = cell
+        return cell
+    }
+
+    func recycle(for key: AnyHashable) {
+        if let cell = inUse.removeValue(forKey: key) {
+            available.append(cell)
+        }
+    }
+
+    func recycleAll(except keys: Set<AnyHashable>) {
+        let toRecycle = inUse.keys.filter { !keys.contains($0) }
+        for key in toRecycle {
+            recycle(for: key)
+        }
+    }
+
+    func reset() {
+        available.append(contentsOf: inUse.values)
+        inUse.removeAll()
+    }
+
+    var activeKeys: Set<AnyHashable> {
+        Set(inUse.keys)
+    }
+}
